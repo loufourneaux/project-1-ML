ef07ba34d265aa6d73b4e7027d2fb6243c15a391
AdrienFeillard <92714776+AdrienFeillard@users.noreply.github.com>
2023-10-21 17:56:40 +0200

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-def remove_col(x) :
+  #Remove columns in which there are too much NaN values (<80%)
+def remove_col(x, nan_percentage = 0.8) :
 
   #Remove columns in which there are too much NaN values (<80%)
-
-  for i in range(x.shape[1] - 1, 2, -1):
-    num_NaN = np.count_nonzero(x[:, i] == -999.000)
+  to_delete = []
+  for i in range(x.shape[1] - 1, 1, -1):
+    num_NaN = np.count_nonzero(np.isnan(x[:,i]))
     p_NaN = num_NaN / x.shape[0]
-    if p_NaN > 0.8:
-      x = np.delete(x, i, 1)
-    return x
+    if p_NaN > nan_percentage:
+      to_delete.append(i)
+
+  x = x[:, [i for i in range(x.shape[1]) if i not in to_delete]]
+  return x
 
 
 
