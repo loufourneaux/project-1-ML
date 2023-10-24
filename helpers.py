2e6c0fc0c27e8a37bb1818bd3837e2a35cd6f4bf
AdrienFeillard <92714776+AdrienFeillard@users.noreply.github.com>
2023-10-24 15:24:16 +0200

    Update helpers.py
    rajout f1_score et split data

--- helpers.py
+++ helpers.py
@@ -76,6 +76,51 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
+def compute_f1_score(y_true, y_pred):
+    tp = np.sum((y_pred == 1) & (y_pred == y_true))
+    print(tp)
+    fp = np.sum((y_pred == 1) & (y_pred != y_true))
+    print(fp)
+    fn = np.sum((y_pred == -1) & (y_pred != y_true))
+    print(fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8
+    you will have 80% of your data set dedicated to training
+    and the rest dedicated to testing. If ratio times the number of samples is not round
+    you can use np.floor. Also check the documentation for np.random.permutation,
+    it could be useful.
+
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        y: numpy array of shape (N,).
+        ratio: scalar in [0,1]
+        seed: integer.
+
+    Returns:
+        x_tr: numpy array containing the train data.
+        x_te: numpy array containing the test data.
+        y_tr: numpy array containing the train labels.
+        y_te: numpy array containing the test labels.
+    """
+    # set seed
+    np.random.seed(seed)
+
+    idx = np.random.permutation(np.arange(len(x)))
+    idx_max = np.floor(ratio * len(x)).astype(int)
+
+    x_tr = x[idx][:idx_max]
+    x_te = x[idx][idx_max:]
+    y_tr = y[idx][:idx_max]
+    y_te = y[idx][idx_max:]
+
+    return x_tr, x_te, y_tr, y_te
+
 def create_csv_submission(ids, y_pred, name):
     """
     This function creates a csv file named 'name' in the format required for a submission in Kaggle or AIcrowd.
