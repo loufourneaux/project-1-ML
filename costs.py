f6fa6bd6c4d56e5011ac549c5cc28c40b41a190f
AdrienFeillard <92714776+AdrienFeillard@users.noreply.github.com>
2023-10-29 18:31:01 +0100

    Update costs.py
    commentaries and handling of y_true for logloss

--- costs.py
+++ costs.py
@@ -1,67 +1,69 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.15.1
-#   kernelspec:
-#     display_name: Python3
-#     language: python
-#     name: python3
-# ---
-
 import numpy as np
 from functions import sigmoid
 
 def calculate_mse(e):
-    """Calculate the mse for vector e."""
+    """
+    Calculate the mean squared error (MSE) for vector e.
+    
+    Args:
+    e (numpy.ndarray): error vector
+    
+    Returns:
+    float: mean squared error
+    """
     return np.mean(e**2) / 2
 
-
 def calculate_mae(e):
-    """Calculate the mae for vector e."""
+    """
+    Calculate the mean absolute error (MAE) for vector e.
+    
+    Args:
+    e (numpy.ndarray): error vector
+    
+    Returns:
+    float: mean absolute error
+    """
     return np.mean(np.abs(e))
 
-
 def calculate_logloss(y_true, y_pred, eps=1e-8):
-    """Calculate the logloss"""
-    return -np.mean(
-        y_true * np.log(y_pred + eps) + (1 - y_true) * np.log(1 - y_pred + eps)
-    )
-
-
+    """
+    Calculate the logistic loss (logloss).
+    
+    Args:
+    y_true (numpy.ndarray): true target values
+    y_pred (numpy.ndarray): predicted target values
+    eps (float, optional): a small number to prevent log(0)
+    
+    Returns:
+    float: logistic loss
+    """
+    y_true = (y_true + 1)/2 # Shift y_true from (-1, 1) to (0, 1)
+    return -np.mean(y_true * np.log(y_pred + eps) + (1 - y_true) * np.log(1 - y_pred + eps))
 
 def compute_loss(y, tx, w, loss_type):
-    """Calculate the loss using either MSE or MAE.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2,). The vector of model parameters.
-        loss_type: string in ["mae", "mse", "log"] specifying the type of loss to compute
-
-    Returns:
-        the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    y = y.reshape(-1,1)
-    w = w.reshape(-1,1)
-    e = y -tx.dot(w)
-
+    Calculate the loss using either MSE, MAE, or logistic loss.
+    
+    Args:
+    y (numpy.ndarray): target values
+    tx (numpy.ndarray): input features
+    w (numpy.ndarray): model weights
+    loss_type (str): specifies the type of loss to compute ("mse", "mae", "log")
+    
+    Returns:
+    float: loss value
+    """
+    y, w = y.reshape(-1, 1), w.reshape(-1, 1)
+    e = y - tx.dot(w)
+    
     if loss_type == "mse":
         return calculate_mse(e)
-
     elif loss_type == "mae":
         return calculate_mae(e)
-
     elif loss_type == "log":
         y_pred = sigmoid(tx @ w)
         return calculate_logloss(y, y_pred)
-
     else:
         raise ValueError(
-            "Invalid value for argument 'loss_type' when calling compute_loss, 'type' must be in ['mse', 'mae', 'log']."
+            "Invalid value for argument 'loss_type'. Must be one of ['mse', 'mae', 'log']."
         )
-
-
