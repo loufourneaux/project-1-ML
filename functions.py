3cc524de72368531d6168d070fc4110eaeb41780
AdrienFeillard <92714776+AdrienFeillard@users.noreply.github.com>
2023-10-29 18:48:01 +0100

    clean

--- functions.py
+++ functions.py
@@ -1,13 +1,5 @@
-# +
-# Useful starting lines
-# %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
-
-# %load_ext autoreload
-# %autoreload 2
-# -
-
 from helpers import *
 from costs import *
 
@@ -29,89 +21,6 @@ def compute_gradient(y, tx, w):
     gradient = (-1/y.shape[0])*np.dot(np.transpose(tx),e)
     return gradient
 
-
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """The Gradient Descent (GD) algorithm.
-
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
-        max_iters: a scalar denoting the total number of iterations of GD
-        gamma: a scalar denoting the stepsize
-
-    Returns:
-        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
-        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of GD
-    """
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        # compute gradient and loss
-        grad = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w, "mse")
-
-        # update w by gradient
-        w = w - gamma * grad
-
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        if n_iter % 20 == 0:
-            print(
-                "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-                    bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
-                )
-            )
-
-    return ws, losses
-
-
-def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """The Stochastic Gradient Descent algorithm (SGD).
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
-        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
-        max_iters: a scalar denoting the total number of iterations of SGD
-        gamma: a scalar denoting the stepsize
-
-    Returns:
-        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
-        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD
-    """
-
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    # batch_size=1
-
-    for n_iter in range(max_iters):
-
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, 1, num_batches=1):
-
-            grad = compute_gradient(minibatch_y, minibatch_tx, w)
-            loss = compute_loss(y, tx, w, "mse")
-
-            w = w - gamma * grad
-
-            ws.append(w)
-            losses.append(loss)
-
-        if n_iter % 20 == 0:
-            print(
-                "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-                    bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
-                )
-            )
-    return ws, losses
-
-
 def sigmoid(t):
     """apply sigmoid function on t.
     Args:
@@ -119,30 +28,6 @@ def sigmoid(t):
     Returns:
         scalar or numpy array"""
     return 1 / (1 + np.exp(-t))
-
-      
-def calculate_gradient(y, tx, w):
-    """compute the gradient of loss.
-
-    Args:
-        y:  shape=(N, 1)
-        tx: shape=(N, D)
-        w:  shape=(D, 1)
-
-    Returns:
-        a vector of shape (D, 1)
-
-    >>> np.set_printoptions(8)
-    >>> y = np.c_[[0., 1.]]
-    >>> tx = np.arange(6).reshape(2, 3)
-    >>> w = np.array([[0.1], [0.2], [0.3]])
-    >>> calculate_gradient(y, tx, w)
-    array([[-0.10370763],
-           [ 0.2067104 ],
-           [ 0.51712843]])
-    """
-    
-    return 1/len(y)*(tx.T.dot(sigmoid(np.dot(tx, w))-y))
     
 def build_poly(x, degree):
     """
